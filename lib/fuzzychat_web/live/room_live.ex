46b44328f5a5dfa6a13654e5687dbcc02a8a18ba
Chris Johnston <cjohnston@connected.io>
2023-06-05 16:46:32 -0400

    Moves html into separate heex file

--- lib/fuzzychat_web/live/room_live.ex
+++ lib/fuzzychat_web/live/room_live.ex
@@ -2,6 +2,7 @@ defmodule FuzzychatWeb.RoomLive do
   @moduledoc """
   Functions for creating and managing chat rooms
   """
+  require Logger
 
   use FuzzychatWeb, :live_view
 
@@ -12,41 +13,7 @@ defmodule FuzzychatWeb.RoomLive do
     {:ok, assign(socket, rooms: rooms, messages: messages)}
   end
 
-  def render(assigns) do
-    ~H"""
-    <div class="grid grid-cols-7 divide-x divide-slate-400">
-      <div class="col-span-1 bg-slate-400 p-4">
-        <h2>Rooms</h2>
-        <ul :for={room <- @rooms}>
-          <li class={if room == "General", do: "selected"}><%= room %></li>
-        </ul>
-      </div>
-      <div class="col-span-6 p-4 bg-gray-100">
-        <div class="align-text-top">
-          <h2>Chat</h2>
-          <div :for={message <- @messages}>
-            <p><%= message %></p>
-          </div>
-          <div class="mt-4 border rounded">
-            <form phx-submit="send-message">
-              <label for="message" class="block">Message</label>
-              <textarea
-                name="message"
-                placeholder="Enter message here"
-                class="rounded block w-full rounded-lg border-gray-300 focus:border-slate-400 focus:ring-slate-400 focus:ring-opacity-50"
-              />
-              <button class="rounded-lg bg-slate-500 py-1 px-5 mt-2 text-white hover:bg-slate-800 float-right">
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    """
-  end
-
   def handle_event("send-message", %{"message" => message}, socket) do
-    {:noreply, assign(socket, :messages, [message | socket.assigns.messages])}
+    {:noreply, assign(socket, :messages, socket.assigns.messages ++ [message])}
   end
 end
