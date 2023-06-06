2b1e19dd9b2af2fb441d6fd85ee32136fe8d464e
Chris Johnston <cjohnston@connected.io>
2023-06-06 14:41:55 -0400

    Adds UI for creating a new room
    - also some styling changes
    - handle events for creating a new room, but no DB yet

--- lib/fuzzychat_web/live/room_live.ex
+++ lib/fuzzychat_web/live/room_live.ex
@@ -7,13 +7,31 @@ defmodule FuzzychatWeb.RoomLive do
   use FuzzychatWeb, :live_view
 
   def mount(_params, _session, socket) do
-    rooms = ["General", "Games", "Cats"]
-    messages = ["this is a message", "this is another message"]
+    rooms = ["General"]
+    messages = []
+    selected_room = "General"
 
-    {:ok, assign(socket, rooms: rooms, messages: messages)}
+    {:ok,
+     assign(socket,
+       rooms: rooms,
+       messages: messages,
+       selected_room: selected_room,
+       new_room: false
+     )}
   end
 
   def handle_event("send-message", %{"message" => message}, socket) do
     {:noreply, assign(socket, :messages, socket.assigns.messages ++ [message])}
+  end
+
+  def handle_event("new-room", _, socket) do
+    {:noreply, assign(socket, :new_room, true)}
+  end
+
+  def handle_event("create-room", %{"room_name" => room_name}, socket) do
+    Logger.info(room_name: room_name)
+    rooms = Enum.sort([room_name | socket.assigns.rooms])
+
+    {:noreply, assign(socket, rooms: rooms, new_room: false)}
   end
 end
